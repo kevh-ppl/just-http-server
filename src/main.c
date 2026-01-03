cf0b930976e073c52cc248859112ce233f1c8ff8
Kevin UDIT <kevinharo.arph@gmail.com>
2026-01-03 15:22:39 -0600

    feat() => Using poll() with accept() to get the server running in a infinite loop

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> //free
 #include <unistd.h> //read
 #include <string.h> //strlen
+#include <poll.h>   //instead of select()
 #include <sys/socket.h>
 
 #include "utils.h"
@@ -33,18 +34,52 @@ int main()
     ssize_t value_read;
     char buffer_stream[1024] = {0};
 
-    if ((client_conn = accept(server_fd, (struct sockaddr *)server.address, &server.addr_len)) < 0)
+    // Trying to use poll syscall from Posix (there's another version from Linux kernel implementation called ppoll() btw)
+    //  i need a loop right here and maybe use select syscall; turn out i'm using poll
+
+    int theres_new_conn;
+    nfds_t num_open_fds, total_nfds;
+    struct pollfd *pfds;
+
+    num_open_fds = total_nfds = 1;
+    // gotta make an array for fds
+    pfds = calloc(total_nfds, sizeof(struct pollfd));
+    if (pfds == NULL)
+        abort_server("Server", "Couldn't allocate memory for pfds\n");
+
+    // i gotta put server_fd in pfds, not client_conn ahhh ._.
+    // so when theres an incoming connections, an readiable event is issued and accept() can be used
+    pfds[0].fd = server_fd;
+    pfds[0].events = POLLIN;
+
+    while (1)
     {
-        print_and_keep_going("Server", "Error accepting connection...%s");
-    };
+        theres_new_conn = poll(pfds, total_nfds, -1);
+        if (theres_new_conn == -1)
+        {
+            // theres an error on poll, can't recover
+            abort_server("Server", "Error during poll");
+        }
+        else if (pfds->revents == 0) // no revents for any fds
+        {
+            continue;
+        }
 
-    value_read = read(client_conn, buffer_stream, 1024 - 1); //-1 because EOF
+        // now i can handle the connection
+        // well, I undertand (may be wrong) that accept() waits for a conn, creates a new socket if there's any
+        // and then returns a fd to that new socket to communicate to the client.
+        if ((client_conn = accept(server_fd, (struct sockaddr *)server.address, &server.addr_len)) < 0)
+        {
+            print_and_keep_going("Server", "Error accepting connection...%s");
+        };
 
-    char *hello = "Hi, this thing works!\n";
-    send(client_conn, hello, strlen(hello), 0);
-    printf("Msg sent\n");
+        value_read = read(client_conn, buffer_stream, 1024 - 1); //-1 because EOF
 
-    close(client_conn);
+        char *hello = "Hi, this thing works!\n";
+        send(client_conn, hello, strlen(hello), 0);
+        printf("Msg sent\n");
+        close(client_conn);
+    }
 
     close(server_fd);
 }
